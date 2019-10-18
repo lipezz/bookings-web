23ad330f683bdcc0b03c5efc50b45be4c9d1be5c
Luiz Menezes <lfmrod@gmail.com>
2019-10-18 16:34:13 -0300

    adding spot and image upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import React, { useState } from 'react';
-import './App.css';
-import api from './services/api';
+import React from 'react';
 
+import './App.css';
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-     
-    const response = await api.post('/sessions', { email });
-  }
 
   return (          
     <div className="container">
       <img src={logo} alt="Bookings" />
 
       <div className="content">
-        <p>
-          Ofereça <b>spots</b> para programadores e encontre <b>talentos</b> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="Seu melhor email" 
-            value={email}
-            onChange={ event => setEmail(event.target.value) }
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
